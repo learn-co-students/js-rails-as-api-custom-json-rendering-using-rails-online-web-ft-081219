e6846575fec5459872172a5b5e838fed986755d7
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-28 10:52:16 -0800

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,32 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # render json: birds
+
+    # to render/show only what we specify:
+    # render json: birds, only: [:id, :name, :species]
+
+    # another way to specify what we want to hide:
+    render json: birds, except: [:created_at, :updated_at]
+    # render json: birds.to_json(except: [:created_at, :updated_at])
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: bird
+
+    # we could just pick and choose what we want to send:
+    # render json: {id: bird.id, name: bird.name, species: bird.species } 
+
+    # another way of specifying what to show:
+    # render json: bird.slice(:id, :name, :species)
+
+
+    # to show any error comes up while showing specific bird:
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
 end
